57ebe2cafab49b863e78dadb638dd83b427762bc
BGCannotCode <93034810+BGCannotCode@users.noreply.github.com>
2023-05-01 18:40:26 +0100

    Update CustomChat.js

--- Global/CustomChat.js
+++ Global/CustomChat.js
@@ -1,14 +1,23 @@
-const newUsername = prompt("What username would you like to give everyone?"); 
-const newPfpUrl = prompt("What is the URL of your new profile pictures?");
+javascript:(function() {
+    var username = prompt("Enter the new username: ");
+    var pfp = prompt("Enter the image URL for the new profile picture: ");
 
-const badges = [    "Ankh",    "Big Spender",    "Booster",    "OG",    "Owner",    "Plus",    "Staff",    "Tester",    "Verified"];
+    var chatNames = document.getElementsByClassName("styles__chatName___F1Z4P-camelCase");
+    for (var i = 0; i < chatNames.length; i++) {
+        var name = chatNames[i];
+        var badges = name.getElementsByTagName("img");
+        while (badges.length > 0) {
+            badges[0].parentNode.removeChild(badges[0]);
+        }
 
-const badgeImgs = badges.map(badge => `<img src="/content/badges/${badge}.png" class="styles__chatBadge___AZ1ZU-camelCase">`).join("");
-
-document.querySelectorAll(".styles__chatAvatar___RZQ83-camelCase").forEach(img => {
-    img.src = newPfpUrl;
-});
-
-document.querySelectorAll(".styles__chatName___F1Z4P-camelCase").forEach(name => {
-    name.innerHTML = `<text style="text-shadow: 0 3px 3px rgba(0, 0, 0, 0.2); color: #FF757E; background: linear-gradient(to right, #FF757E, #FF977E, #FFB97E, #FFDB7E, #FFFF7E); -webkit-background-clip: text; -webkit-text-fill-color: transparent;">${newUsername}</text>${badgeImgs}`;
-});
+        name.style.color = "white";
+        name.style.textShadow = "0 3px 3px rgba(0, 0, 0, 0.2)";
+        name.innerHTML = "<span style='background-image: linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet); -webkit-background-clip: text; -webkit-text-fill-color: transparent;'>" + username + "</span>";
+        
+        var pfpElement = name.previousElementSibling;
+        pfpElement.setAttribute("src", pfp);
+        
+        var badgesHTML = "<img src='/content/badges/Ankh.png' class='styles__chatBadge___AZ1ZU-camelCase'><img src='/content/badges/OG.png' class='styles__chatBadge___AZ1ZU-camelCase'><img src='/content/badges/Owner.png' class='styles__chatBadge___AZ1ZU-camelCase'><img src='/content/badges/Plus.png' class='styles__chatBadge___AZ1ZU-camelCase'><img src='/content/badges/Staff.png' class='styles__chatBadge___AZ1ZU-camelCase'><img src='/content/badges/Tester.png' class='styles__chatBadge___AZ1ZU-camelCase'><img src='/content/badges/Verified.png' class='styles__chatBadge___AZ1ZU-camelCase'><img src='https://media.discordapp.net/attachments/1036199159969103872/1102637372945993798/image-removebg-preview_27.png?width=645&height=603' class='styles__chatBadge___AZ1ZU-camelCase'>";
+        name.innerHTML += badgesHTML;
+    }
+})();
