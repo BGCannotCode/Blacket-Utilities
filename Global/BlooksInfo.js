cd6ee2ddd8b29af2877bad34ade8ef20a8b82f57
BGCannotCode <93034810+BGCannotCode@users.noreply.github.com>
2023-05-08 19:20:06 +0100

    Update BlooksInfo.js

--- Global/BlooksInfo.js
+++ Global/BlooksInfo.js
@@ -1,13 +1,21 @@
-console.log('%cObtained:', 'font-size: 18px; font-weight: bold;');
-let obtainedStr = '';
-blacket.packBlooks.forEach(a => {
-    if (Object.keys(blacket.user.blooks).includes(a)) obtainedStr += a + '\n';
-})
-console.log(obtainedStr);
+blacket.requests.get("/worker/packs", (data) => {
+  blacket.packs = data.packs;
+  blacket.packBlooks = [];
+  Object.keys(blacket.packs).forEach(pack => {
+    Object.entries(blacket.packs[pack].blooks).forEach((blook) => blacket.packBlooks.push(blook[1]));
+  })
 
-console.log('%cMissing:', 'font-size: 18px; font-weight: bold;');
-let missingStr = '';
-blacket.packBlooks.forEach(a => {
+  console.log('%cObtained:', 'font-size: 18px; font-weight: bold;');
+  let obtainedStr = '';
+  blacket.packBlooks.forEach(a => {
+    if (Object.keys(blacket.user.blooks).includes(a)) obtainedStr += a + '\n';
+  })
+  console.log(obtainedStr);
+
+  console.log('%cMissing:', 'font-size: 18px; font-weight: bold;');
+  let missingStr = '';
+  blacket.packBlooks.forEach(a => {
     if (!Object.keys(blacket.user.blooks).includes(a)) missingStr += a + '\n';
+  })
+  console.log(missingStr);
 })
-console.log(missingStr);
